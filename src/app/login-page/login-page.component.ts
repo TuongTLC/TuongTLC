4f8ecd2272c2f1b7405fcb07bc77a8cc32c108be
TuongTLC <53476330+TuongTLC@users.noreply.github.com>
2023-09-01 03:58:49 +0700

    login error

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -15,20 +15,42 @@ export class LoginPageComponent {
   };
   userInfo: any;
   loginError: any;
+  usernameEmpty: any;
+  passwordEmpty: any;
   constructor(private userService: UserService, private router: Router) {}
+  validateLoginInfo() {
+    if (this.loginInfo.username?.length == 0) {
+      this.usernameEmpty = true;
+    }
+    if (this.loginInfo.password?.length == 0) {
+      this.passwordEmpty = true;
+    }
+    if (this.usernameEmpty === true || this.passwordEmpty === true) {
+      return false;
+    } else {
+      return true;
+    }
+  }
   login() {
+    this.usernameEmpty = false;
+    this.passwordEmpty = false;
     this.loginError = null;
-    this.userInfo = null ;
+    this.userInfo = null;
+
+    if (this.validateLoginInfo() == false) {
+      return;
+    }
+
     this.userService.login(this.loginInfo).subscribe({
-      next : res => {
+      next: (res) => {
         this.userInfo = res;
-        if(this.userInfo){
+        if (this.userInfo) {
           this.router.navigate(['/home']);
         }
-      }, error: (error) =>{
+      },
+      error: (error) => {
         this.loginError = error;
-      }
-    })
-    
+      },
+    });
   }
 }
