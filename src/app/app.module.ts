c48bd98d7e6c301406ff012a70369c3fa1f77418
TuongTLC <53476330+TuongTLC@users.noreply.github.com>
2023-09-13 23:24:34 +0700

    test editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { HomePageComponent } from './home-page/home-page.component';
-import { ErrorPageComponent } from './error-page/error-page.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { WelcomePageComponent } from './welcome-page/welcome-page.component';
-import { TutorialsPageComponent } from './tutorials-page/tutorials-page.component';
-import { TipsPageComponent } from './tips-page/tips-page.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { RegisterPageComponent } from './register-page/register-page.component';
-import { AboutPageComponent } from './about-page/about-page.component';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { PopupModalComponent } from './popup-modal/popup-modal.component';
-import { PasswordPageComponent } from './password-page/password-page.component';
-import { UserInfoPageComponent } from './user-info-page/user-info-page.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {HomePageComponent} from './home-page/home-page.component';
+import {ErrorPageComponent} from './error-page/error-page.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {WelcomePageComponent} from './welcome-page/welcome-page.component';
+import {TutorialsPageComponent} from './tutorials-page/tutorials-page.component';
+import {TipsPageComponent} from './tips-page/tips-page.component';
+import {LoginPageComponent} from './login-page/login-page.component';
+import {RegisterPageComponent} from './register-page/register-page.component';
+import {AboutPageComponent} from './about-page/about-page.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {PopupModalComponent} from './popup-modal/popup-modal.component';
+import {PasswordPageComponent} from './password-page/password-page.component';
+import {UserInfoPageComponent} from './user-info-page/user-info-page.component';
+import {NgxEditorModule} from "ngx-editor";
+import {SanitizeHtmlPipes} from "./sanitizer";
+
 @NgModule({
   declarations: [
     WelcomePageComponent,
@@ -30,9 +33,11 @@ import { UserInfoPageComponent } from './user-info-page/user-info-page.component
     PopupModalComponent,
     PasswordPageComponent,
     UserInfoPageComponent,
+    SanitizeHtmlPipes
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, NgxEditorModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [NavbarComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
+import {FormControl, FormGroup} from "@angular/forms";
+import jsonDoc from "../doc";
+import {Editor, toHTML, Toolbar, Validators} from "ngx-editor";
 
 @Component({
   selector: 'app-home-page',
@@ -7,11 +10,45 @@ import { Component } from '@angular/core';
 })
 export class HomePageComponent {
   userInfo: any;
-  constructor() {}
+  editordoc: any;
+  editor!: Editor;
+  toolbar: Toolbar = [
+    ['bold', 'italic'],
+    ['underline', 'strike'],
+    ['code', 'blockquote'],
+    ['ordered_list', 'bullet_list'],
+    [{heading: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']}],
+    ['link', 'image'],
+    ['text_color', 'background_color'],
+    ['align_left', 'align_center', 'align_right', 'align_justify'],
+  ];
+
+  form = new FormGroup({
+    editorContent: new FormControl(
+      {value: jsonDoc, disabled: false},
+      Validators.required()
+    ),
+  });
+
+  saveContent: any;
+
+  save() {
+    this.editordoc = this.form.get('editorContent')?.value;
+    this.saveContent = toHTML(this.editordoc);
+    console.log(this.saveContent)
+  }
+
   ngOnInit(): void {
+    this.editor = new Editor();
+    this.editordoc = jsonDoc;
     const userJson = sessionStorage.getItem('userInfo');
     if (userJson !== null) {
       this.userInfo = JSON.parse(userJson);
     }
   }
+
+  ngOnDestroy(): void {
+    this.editor.destroy();
+  }
+
 }
